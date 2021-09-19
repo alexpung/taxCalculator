812f003417a60ef0d70a391a1ed1094eff34565f
Alex Pun <alexpung@protonmail.com>
2021-09-19 10:14:04 +0100

    refactor excel formatting code to new class ExcelFormatter to reuse

--- dividend/dividend_data_format.py
+++ dividend/dividend_data_format.py
@@ -1,29 +1,15 @@
 """
 Collection of excel sheet formatting for writing Pandas Dataframe to excel
 """
-from openpyxl.styles import Alignment
+from openpyxl.worksheet.worksheet import Worksheet
+from excel_format.format import ExcelFormatter
 
 
-def format_dividend_data(ws) -> None:
+def format_dividend_data(ws: Worksheet) -> None:
     """
     excel formatting for dividend data sheet
     """
-    # Set format for dividend and withholding tax
-    for row in ws['F:I']:
-        for cell in row:
-            cell.number_format = '#,##0.00'
-    # Set format for settleDate
-    for cell in ws['A']:
-        cell.number_format = 'DD/MM/YY'
-    # calculate suitable column width length+2 * 1.2
-    dims = {}
-    for row in ws.rows:
-        for cell in row:
-            if cell.value:
-                dims[cell.column_letter] = max((dims.get(cell.column_letter, 0), (len(str(cell.value))+2) * 1.2))
-    for col, value in dims.items():
-        ws.column_dimensions[col].width = value
-    # Set center alignment for row A to D
-    for row in ws['A:D']:
-        for cell in row:
-            cell.alignment = Alignment(horizontal='center')
+    ExcelFormatter.format_cell(ws, 'A', 'DD/MM/YY')
+    ExcelFormatter.format_cell(ws, 'F:I', '#,##0.00')
+    ExcelFormatter.auto_size_column(ws)
+    ExcelFormatter.set_alignment(ws, 'A:D', 'center')
