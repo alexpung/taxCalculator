f14acb1c11612eb4e0a379469ae512d742399754
Alex Pun <alexpung@protonmail.com>
2021-12-17 20:07:02 +0000

    refactor constants into classes.

--- dividend/const.py
+++ dividend/const.py
@@ -1,36 +1,57 @@
 """
 xml column names and output names
 """
-# excel sheet names
-DIVIDEND_SUMMARY = "dividend summary"
-DIVIDEND_DATA = "dividend data"
-INTEREST_DATA = 'interest data'
+
 
 # interactive brokers xml column names
-CURRENCY = 'currency'
-LISTING_EXCHANGE = 'listingExchange'
-SYMBOL = 'symbol'
-TYPE = 'type'
-FX_RATE_TO_BASE = 'fxRateToBase'
-GROSS_DIVIDEND = 'Gross dividend'
-AMOUNT = 'amount'
-SETTLE_DATE = 'settleDate'
-ISIN = 'isin'
-DESCRIPTION = 'description'
+class ColumnName(str):
+    """Keep track of the column names both imported from xml statement and output"""
+
+    CURRENCY = "currency"
+    LISTING_EXCHANGE = "listingExchange"
+    SYMBOL = "symbol"
+    TYPE = "type"
+    FX_RATE_TO_BASE = "fxRateToBase"
+    GROSS_DIVIDEND = "Gross dividend"
+    AMOUNT = "amount"
+    SETTLE_DATE = "settleDate"
+    ISIN = "isin"
+    DESCRIPTION = "description"
+    TAX_IN_STERLING = "Withholding Tax in Sterling"
+    DIVIDEND_IN_STERLING = "Gross Dividend in Sterling"
+    INTEREST_IN_STERLING = "Gross Interest in Sterling"
+    COUNTRY = "country"
+
 
 # interactive brokers cash transaction type
-IN_LIEU_OF_DIVIDENDS = "Payment In Lieu Of Dividends"
-WITHHOLDING_TAX = 'Withholding Tax'
-DIVIDENDS = "Dividends"
-DIVIDEND_TYPE = [IN_LIEU_OF_DIVIDENDS, WITHHOLDING_TAX, DIVIDENDS]
-BOND_INTEREST = "Bond Interest Received"
-BROKER_INTEREST_RECEIVED = "Broker Interest Received"
-BROKER_INTEREST_PAID = "Broker Interest Paid"
-INTEREST_INCOME = [BOND_INTEREST, BROKER_INTEREST_RECEIVED]
+class TransactionType(str):
+    """Keep track of the possible transaction type"""
 
-# output names
-TAX_IN_STERLING = 'Withholding Tax in Sterling'
-DIVIDEND_IN_STERLING = 'Gross Dividend in Sterling'
-INTEREST_IN_STERLING = 'Gross Interest in Sterling'
-COUNTRY = 'country'
-INTEREST_COLUMN = [SETTLE_DATE, DESCRIPTION, TYPE, AMOUNT, CURRENCY, FX_RATE_TO_BASE, INTEREST_IN_STERLING]
+    IN_LIEU_OF_DIVIDENDS = "Payment In Lieu Of Dividends"
+    WITHHOLDING_TAX = "Withholding Tax"
+    DIVIDENDS = "Dividends"
+    BOND_INTEREST = "Bond Interest Received"
+    BROKER_INTEREST_RECEIVED = "Broker Interest Received"
+    BROKER_INTEREST_PAID = "Broker Interest Paid"
+
+
+INTEREST_COLUMN = [
+    ColumnName.SETTLE_DATE,
+    ColumnName.DESCRIPTION,
+    ColumnName.TYPE,
+    ColumnName.AMOUNT,
+    ColumnName.CURRENCY,
+    ColumnName.FX_RATE_TO_BASE,
+    ColumnName.INTEREST_IN_STERLING,
+]
+
+INTEREST_INCOME = [
+    TransactionType.BOND_INTEREST,
+    TransactionType.BROKER_INTEREST_RECEIVED,
+]
+
+DIVIDEND_TYPE = [
+    TransactionType.IN_LIEU_OF_DIVIDENDS,
+    TransactionType.WITHHOLDING_TAX,
+    TransactionType.DIVIDENDS,
+]
