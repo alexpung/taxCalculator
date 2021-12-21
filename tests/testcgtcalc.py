846048d606e55c2dbe60e7dbf0d922bf504dac78
Alex Pun <alexpung@protonmail.com>
2021-12-21 12:36:03 +0000

    Add test expected results Change test cases due to change of data structure of Transaction class Correct ordering of assert equal so expected result is 1st argument and unit under test is 2nd argument

--- tests/testcgtcalc.py
+++ tests/testcgtcalc.py
@@ -1,17 +1,41 @@
 """ testing for capital gain matching module """
 import datetime
 from decimal import Decimal
+from typing import Tuple
 import unittest
 
 from capital_gain.exception import MixedTickerError
-from capital_gain.model import CgtCalculator, Transaction, TransactionType
+from capital_gain.model import CgtCalculator, MatchType, Transaction, TransactionType
 
 
 class TestCalculator(unittest.TestCase):
     """To test that capital gain is calculated correctly"""
 
+    @staticmethod
+    def _sum_match_type(transaction: Transaction) -> Tuple[Decimal, Decimal, Decimal]:
+        bednbreakfast = sum(
+            [
+                match.size
+                for match in transaction.match_status.record
+                if match.type == MatchType.BED_AND_BREAKFAST
+            ],
+            Decimal(0),
+        )
+        sameday = sum(
+            [
+                match.size
+                for match in transaction.match_status.record
+                if match.type == MatchType.SAME_DAY
+            ],
+            Decimal(0),
+        )
+        return transaction.match_status.unmatched, sameday, bednbreakfast
+
     def test_mixed_ticker(self) -> None:
-        """To test that an error is raised when transaction list has mixed ticker"""
+        """To test that an error is raised when transaction list has mixed ticker
+
+        Expected result: Raise MixedTickerError since different Ticker is mixed in.
+        """
         trades = [
             Transaction(
                 "AMD",
@@ -38,7 +62,10 @@ class TestCalculator(unittest.TestCase):
         self.assertRaises(MixedTickerError, CgtCalculator, trades)
 
     def test_unmixed_ticker(self) -> None:
-        """To test that the calculator do not raise an error when no mixed tickers"""
+        """To test that the calculator do not raise an error when no mixed tickers
+
+        Expected result: No MixedTickerError is raised
+        """
         trades = [
             Transaction(
                 "AMD",
@@ -68,7 +95,11 @@ class TestCalculator(unittest.TestCase):
             self.fail("MixedTickerError raised with same tracker.")
 
     def test_same_day_matching(self) -> None:
-        """To test that same day matching function works"""
+        """To test that same day matching function works
+
+        Expected result: 3rd, 4th and 5th transaction will match and leaving 30 shares
+        that were sold unmatched
+        """
         trades = [
             Transaction(
                 "AMD",
@@ -115,22 +146,39 @@ class TestCalculator(unittest.TestCase):
         ]
         test = CgtCalculator(trades)
         test.match_same_day_disposal()
-        self.assertEqual(Decimal(100), test.transaction_list[0].match_status.unmatched)
-        self.assertEqual(Decimal(0), test.transaction_list[0].match_status.same_day)
-        self.assertEqual(Decimal(110), test.transaction_list[1].match_status.unmatched)
-        self.assertEqual(Decimal(0), test.transaction_list[1].match_status.same_day)
-        self.assertEqual(Decimal(0), test.transaction_list[2].match_status.unmatched)
-        self.assertEqual(Decimal(100), test.transaction_list[2].match_status.same_day)
-        self.assertEqual(Decimal(30), test.transaction_list[3].match_status.unmatched)
-        self.assertEqual(Decimal(120), test.transaction_list[3].match_status.same_day)
-        self.assertEqual(Decimal(0), test.transaction_list[4].match_status.unmatched)
-        self.assertEqual(Decimal(20), test.transaction_list[4].match_status.same_day)
-        self.assertEqual(Decimal(100), test.transaction_list[5].match_status.unmatched)
-        self.assertEqual(Decimal(0), test.transaction_list[5].match_status.same_day)
+        self.assertEqual(
+            self._sum_match_type(test.transaction_list[0]),
+            (Decimal(100), Decimal(0), Decimal(0)),
+        )
+        self.assertEqual(
+            self._sum_match_type(test.transaction_list[1]),
+            (Decimal(110), Decimal(0), Decimal(0)),
+        )
+        self.assertEqual(
+            self._sum_match_type(test.transaction_list[2]),
+            (Decimal(0), Decimal(100), Decimal(0)),
+        )
+        self.assertEqual(
+            self._sum_match_type(test.transaction_list[3]),
+            (Decimal(30), Decimal(120), Decimal(0)),
+        )
+        self.assertEqual(
+            self._sum_match_type(test.transaction_list[4]),
+            (Decimal(0), Decimal(20), Decimal(0)),
+        )
+        self.assertEqual(
+            self._sum_match_type(test.transaction_list[5]),
+            (Decimal(100), Decimal(0), Decimal(0)),
+        )
 
     def test_bed_and_breakfast_matching(self) -> None:
         """To test bread and breakfast matching works and
-        match buy transaction within 30 days of a sell"""
+        match buy transaction within 30 days of a sell
+
+        Expected result: 3rd transaction will match with 2nd transaction
+        6th transaction will match with 5th transaction
+        4th and 7th transaction will not match as they are outside 30 days limit
+        """
         trades = [
             Transaction(
                 "AMD",
@@ -184,37 +232,40 @@ class TestCalculator(unittest.TestCase):
         ]
         test = CgtCalculator(trades)
         test.match_bed_and_breakfast_disposal()
-        self.assertEqual(Decimal(100), test.transaction_list[0].match_status.unmatched)
         self.assertEqual(
-            Decimal(0), test.transaction_list[0].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[0]),
+            (Decimal(100), Decimal(0), Decimal(0)),
         )
-        self.assertEqual(Decimal(30), test.transaction_list[1].match_status.unmatched)
         self.assertEqual(
-            Decimal(20), test.transaction_list[1].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[1]),
+            (Decimal(30), Decimal(0), Decimal(20)),
         )
-        self.assertEqual(Decimal(0), test.transaction_list[2].match_status.unmatched)
         self.assertEqual(
-            Decimal(20), test.transaction_list[2].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[2]),
+            (Decimal(0), Decimal(0), Decimal(20)),
         )
-        self.assertEqual(Decimal(30), test.transaction_list[3].match_status.unmatched)
         self.assertEqual(
-            Decimal(0), test.transaction_list[3].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[3]),
+            (Decimal(30), Decimal(0), Decimal(0)),
         )
-        self.assertEqual(Decimal(10), test.transaction_list[4].match_status.unmatched)
         self.assertEqual(
-            Decimal(10), test.transaction_list[4].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[4]),
+            (Decimal(10), Decimal(0), Decimal(10)),
         )
-        self.assertEqual(Decimal(0), test.transaction_list[5].match_status.unmatched)
         self.assertEqual(
-            Decimal(10), test.transaction_list[5].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[5]),
+            (Decimal(0), Decimal(0), Decimal(10)),
         )
-        self.assertEqual(Decimal(10), test.transaction_list[6].match_status.unmatched)
         self.assertEqual(
-            Decimal(0), test.transaction_list[6].match_status.bed_and_breakfast
+            self._sum_match_type(test.transaction_list[6]),
+            (Decimal(10), Decimal(0), Decimal(0)),
         )
 
     def test_sorted(self) -> None:
-        """To test the sort function sort transaction list by date"""
+        """To test the sort function sort transaction list by date
+
+        Expected result: Transactions are sorted in chronological order
+        """
         trades = [
             Transaction(
                 "AMD",
