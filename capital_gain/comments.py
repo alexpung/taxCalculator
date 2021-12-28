f30235865c5685a53a68416448156ff15304bdf5
Alex Pun <alexpung@protonmail.com>
2021-12-28 13:34:42 +0000

    Refactor comment class to hold all the comment strings as function returns

--- capital_gain/comments.py
+++ capital_gain/comments.py
@@ -1,42 +1,33 @@
 """ Strings representation of the comment section in transactions """
 
-from dataclasses import dataclass
 from decimal import Decimal
 
 
-@dataclass(repr=False)
-class AddToSection104:
-    """Comment for adding a buy transaction to the Section 104 pool"""
+class Comment:
+    """Comment class for commenting transactions"""
 
-    old_qty: Decimal
-    old_cost: Decimal
-    added_qty: Decimal
-    added_cost: Decimal
-
-    def __repr__(self) -> str:
+    @staticmethod
+    def add_to_section104(
+        qty: Decimal, cost: Decimal, new_quantity: Decimal, new_cost: Decimal
+    ) -> str:
+        """Comment when adding shares to the Section 104 pool"""
         return (
-            f"{self.added_qty} share(s) added to Section104 pool "
-            f"with allowable cost {self.added_cost}.\n"
+            f"{qty} share(s) added to Section104 pool "
+            f"with allowable cost {cost}.\n"
             f"New total number of share(s) for section 104 "
-            f"is {self.added_qty+self.old_qty}.\n"
-            f"New total allowable cost is {self.added_cost + self.old_cost}"
+            f"is {new_quantity}.\n"
+            f"New total allowable cost is {new_cost}"
         )
 
-
-@dataclass(repr=False)
-class RemoveFromSection104:
-    """Comment for adding a buy transaction to the Section 104 pool"""
-
-    old_qty: Decimal
-    old_cost: Decimal
-    removed_qty: Decimal
-    removed_cost: Decimal
-
-    def __repr__(self) -> str:
+    @staticmethod
+    def remove_from_section104(
+        qty: Decimal, cost: Decimal, new_quantity: Decimal, new_cost: Decimal
+    ) -> str:
+        """Comment when removing shares to the Section 104 pool"""
         return (
-            f"{self.removed_qty} share(s) removed to Section104 pool "
-            f"with allowable cost {self.removed_cost}.\n"
+            f"{qty} share(s) removed to Section104 pool "
+            f"with allowable cost {cost}.\n"
             f"New total number of share(s) for section 104 "
-            f"is {self.removed_qty+self.old_qty}.\n"
-            f"New total allowable cost is {self.removed_cost + self.old_cost}"
+            f"is {new_quantity}.\n"
+            f"New total allowable cost is {new_cost}"
         )
