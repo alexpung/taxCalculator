33fccef4673b8e4de0eb51a041f4f63c97a514d6
Alex Pun <alexpung@protonmail.com>
2021-12-28 11:47:11 +0000

    Add string representation for remove from section 104

--- capital_gain/comments.py
+++ capital_gain/comments.py
@@ -21,3 +21,22 @@ class AddToSection104:
             f"is {self.added_qty+self.old_qty}.\n"
             f"New total allowable cost is {self.added_cost + self.old_cost}"
         )
+
+
+@dataclass(repr=False)
+class RemoveFromSection104:
+    """Comment for adding a buy transaction to the Section 104 pool"""
+
+    old_qty: Decimal
+    old_cost: Decimal
+    removed_qty: Decimal
+    removed_cost: Decimal
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.removed_qty} share(s) removed to Section104 pool "
+            f"with allowable cost {self.removed_cost}.\n"
+            f"New total number of share(s) for section 104 "
+            f"is {self.removed_qty+self.old_qty}.\n"
+            f"New total allowable cost is {self.removed_cost + self.old_cost}"
+        )
