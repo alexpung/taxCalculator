20083c368606a55a1556c41741fcf2e527b4190b
Alex Pun <alexpung@protonmail.com>
2022-01-24 15:52:08 +0000

    Layout and code structure overhaul - move import/export and tax year control to top - Added custom date selector (underlying not implemented) - Remove bad code relying on "remembering" the object instance.

--- gui/test.py
+++ gui/test.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import datetime
 from glob import glob
 from tkinter import Tk, filedialog
-from typing import Any, List, Tuple
+from typing import Any, Final, List, Tuple
 
 from kivy.config import Config
 from kivy.lang import Builder
@@ -13,14 +13,19 @@ from kivy.metrics import dp
 # pylint: disable=no-name-in-module
 from kivy.properties import ListProperty, StringProperty
 from kivymd.app import MDApp
+from kivymd.toast import toast
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.label import MDLabel
 from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.pickers import MDDatePicker
 
 from capital_gain.calculator import CgtCalculator
 from capital_gain.model import Dividend, Section104, Trade
 from statement_parser.ibkr import parse_dividend, parse_trade
+
+# pylint bug, disable checking kivy.properties
+# pylint: disable=no-name-in-module
 
 Config.set("input", "mouse", "mouse,multitouch_on_demand")
 
@@ -29,6 +34,7 @@ class TaxYearWidget(MDBoxLayout):
     """Layout containing the control of tax year selection"""
 
     tax_year = StringProperty()
+    LABEL_CUSTOM: Final[str] = "Custom"
     """ To control the display of the tax year """
 
     def __init__(self, **kwargs):
@@ -38,14 +44,14 @@ class TaxYearWidget(MDBoxLayout):
 
     def on_press_left(self):
         """left button pressed: subtract a year"""
-        if self.tax_year == "Custom":
+        if self.tax_year == self.LABEL_CUSTOM:
             self.tax_year = str(datetime.datetime.now().year)
         else:
             self.tax_year = str(int(self.tax_year) - 1)
 
     def on_press_right(self):
         """right button pressed: add a year"""
-        if self.tax_year == "Custom":
+        if self.tax_year == self.LABEL_CUSTOM:
             self.tax_year = str(datetime.datetime.now().year)
         else:
             self.tax_year = str(int(self.tax_year) + 1)
@@ -55,80 +61,22 @@ class TaxYearWidget(MDBoxLayout):
         """helper function to get start and end date of tax year"""
         return datetime.date(year, 4, 6), datetime.date(year + 1, 4, 5)
 
+    def on_save(self, _1, _2, date_range):
+        """Events called when the "OK" dialog box button is clicked."""
+        if len(date_range) < 2:
+            toast("Please select a valid date range with start and end date")
+        else:
+            self.start_date = date_range[0]
+            print(self.start_date)
+            self.end_date = date_range[-1]
+            print(self.end_date)
+            self.tax_year = self.LABEL_CUSTOM
 
-class CalculatorControl:
-    """Control functions for the app"""
-
-    def __init__(self) -> None:
-        self.trades: List[Trade] = []
-        self.dividends: List[Dividend] = []
-        self.section104: List[Section104] = []
-
-    def calculate(self) -> None:
-        """invoke calculation of capital gain"""
-        trade_bucket = defaultdict(list)
-        # put trades with the same symbol together and calculate tax
-        for trade in self.trades:
-            trade_bucket[trade.ticker].append(trade)
-        for _, trade_list in trade_bucket.items():
-            section104_single = CgtCalculator(trade_list).calculate_tax()
-            if section104_single.quantity > 0:
-                self.section104.append(section104_single)
-
-    def import_file(self, file: str) -> None:
-        """Import a single file"""
-        self.trades += parse_trade(file)
-        self.trades = sorted(self.trades, key=lambda x: (x.ticker, x.transaction_date))
-        self.dividends += parse_dividend(file)
-        self.calculate()
-        for trade in self.trades:
-            if DataTable.last_instance:
-                DataTable.last_instance.add_row(trade.get_tuple_repr())
-
-    def import_folder(self, path: str) -> None:
-        """Import a directory"""
-        # hardcoded to import XML file
-        for file in glob(path + "/*.xml"):
-            self.trades += parse_trade(file)
-            self.trades = sorted(
-                self.trades, key=lambda x: (x.ticker, x.transaction_date)
-            )
-            self.dividends += parse_dividend(file)
-        self.calculate()
-        for trade in self.trades:
-            if DataTable.last_instance:
-                DataTable.last_instance.add_row(trade.get_tuple_repr())
-
-
-# pylint: disable=too-many-ancestors
-class CalculationDetailLabel(MDLabel):
-    """Label to show the capital gain calculation details"""
-
-    last_instance = None
-
-    def __init__(self, **kwargs: Any):
-        super().__init__(**kwargs)
-        CalculationDetailLabel.last_instance = self
-
-
-class DataTable(MDDataTable):
-    """Table to show the trade transactions"""
-
-    last_instance = None
-    column_data = [
-        ("ID", dp(30)),
-        ("Symbol", dp(30)),
-        ("Transaction Date", dp(30)),
-        ("Transaction Type", dp(30)),
-        ("Quantity", dp(30)),
-        ("Gross Value", dp(30)),
-        ("Allowable fees and Taxes", dp(30)),
-        ("Capital gain (loss)", dp(30)),
-    ]
-
-    def __init__(self, **kwargs: Any):
-        super().__init__(**kwargs)
-        DataTable.last_instance = self
+    def show_date_picker(self):
+        """show the custom tax date picker"""
+        date_dialog = MDDatePicker(mode="range", min_year=2000)
+        date_dialog.bind(on_save=self.on_save)
+        date_dialog.open()
 
 
 class TableLayout(MDBoxLayout):
@@ -139,9 +87,24 @@ class TableLayout(MDBoxLayout):
 
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
+        column_data = [
+            ("ID", dp(30)),
+            ("Symbol", dp(30)),
+            ("Transaction Date", dp(30)),
+            ("Transaction Type", dp(30)),
+            ("Quantity", dp(30)),
+            ("Gross Value", dp(30)),
+            ("Allowable fees and Taxes", dp(30)),
+            ("Capital gain (loss)", dp(30)),
+        ]
+        self.table = MDDataTable(
+            use_pagination=True, rows_num=10, column_data=column_data
+        )
         label = MDLabel(text="Transaction List", size_hint=(1, 0.1))
+        self.table.bind(on_row_press=MDApp.get_running_app().on_row_press)
+        MDApp.get_running_app().bind(trade_table_data=self.table.update_row_data)
         self.add_widget(label)
-        self.add_widget(DataTable.last_instance)
+        self.add_widget(self.table)
 
 
 class ItemDrawer(OneLineIconListItem):
@@ -154,11 +117,14 @@ class ItemDrawer(OneLineIconListItem):
 class CalculatorApp(MDApp):
     """Main application"""
 
+    trade_description = StringProperty()
+    trade_table_data = ListProperty()
+
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        self.calculator_control = CalculatorControl()
-        self.table = DataTable(size_hint=(1, 0.9), use_pagination=True, rows_num=10)
-        self.table.bind(on_row_press=self.on_row_press)
+        self.trades: List[Trade] = []
+        self.dividends: List[Dividend] = []
+        self.section104: List[Section104] = []
 
     def build(self) -> Any:
         return Builder.load_file("kivymd.kv")
@@ -169,7 +135,7 @@ class CalculatorApp(MDApp):
         root.withdraw()
         file_path = filedialog.askdirectory()
         if file_path != "":
-            self.calculator_control.import_folder(file_path)
+            self.import_folder(file_path)
 
     def select_file(self) -> None:
         """Invoke Tinker for selecting an import file"""
@@ -177,20 +143,41 @@ class CalculatorApp(MDApp):
         root.withdraw()
         file_name = filedialog.askopenfilename()
         if file_name != "":
-            self.calculator_control.import_file(file_name)
+            self.import_file(file_name)
+
+    def calculate(self, file: str) -> None:
+        """invoke calculation of capital gain"""
+        self.trades += parse_trade(file)
+        self.dividends += parse_dividend(file)
+        trade_bucket = defaultdict(list)
+        # put trades with the same symbol together and calculate tax
+        for trade in self.trades:
+            trade_bucket[trade.ticker].append(trade)
+        for _, trade_list in trade_bucket.items():
+            section104_single = CgtCalculator(trade_list).calculate_tax()
+            if section104_single.quantity > 0:
+                self.section104.append(section104_single)
+        # sort the results and put it in the table
+        self.trades = sorted(self.trades, key=lambda x: (x.ticker, x.transaction_date))
+        self.trade_table_data = [trade.get_tuple_repr() for trade in self.trades]
+
+    def import_file(self, file: str) -> None:
+        """Import a single file"""
+        self.calculate(file)
+
+    def import_folder(self, path: str) -> None:
+        """Import a directory"""
+        # hardcoded to import XML file
+        for file in glob(path + "/*.xml"):
+            self.calculate(file)
 
     def on_row_press(self, _, instance_row) -> None:
         """Called when a table row is clicked."""
         start_index, _ = instance_row.table.recycle_data[instance_row.index]["range"]
         index = int(instance_row.table.recycle_data[start_index]["text"])
-        target = next(
-            trade
-            for trade in self.calculator_control.trades
-            if trade.transaction_id == index
+        self.trade_description = str(
+            next(trade for trade in self.trades if trade.transaction_id == index)
         )
-        print(target)
-        if CalculationDetailLabel.last_instance:
-            CalculationDetailLabel.last_instance.text = str(target)
 
 
 if __name__ == "__main__":
