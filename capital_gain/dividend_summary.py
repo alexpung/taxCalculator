ade0153031088697982ce3cbbb12493ed8e34396
Alex Pun <alexpung@protonmail.com>
2022-02-03 10:48:40 +0000

    Implement basic dividend summary

---
+++ capital_gain/dividend_summary.py
@@ -0,0 +1,95 @@
+""" To calculate various dividend summary """
+from collections import defaultdict
+import datetime
+from decimal import Decimal
+from typing import List
+
+from capital_gain.model import Dividend, TransactionType
+
+
+class DividendSummary:
+    """calculate summary of dividend and withholding tax received during the period"""
+
+    def __init__(
+        self,
+        dividend_list: List[Dividend],
+        tax_year_start: datetime.date,
+        tax_year_end: datetime.date,
+    ):
+        self.dividend_list = list(
+            filter(
+                lambda dividend: tax_year_end
+                >= dividend.transaction_date
+                >= tax_year_start,
+                dividend_list,
+            )
+        )
+        self.tax_year_start = tax_year_start
+        self.tax_year_end = tax_year_end
+
+    def get_dividend_by_country(self) -> defaultdict[str, List[Dividend]]:
+        """get dividend and withholding tax by country code"""
+        dividend_by_country: defaultdict[str, List[Dividend]] = defaultdict(list)
+        for dividend in self.dividend_list:
+            dividend_by_country[dividend.country].append(dividend)
+        return dividend_by_country
+
+    def show_dividend_total(self) -> str:
+        """show total dividends"""
+        dividend = self.get_dividend_total(self.dividend_list, TransactionType.DIVIDEND)
+        dividend_in_lieu = self.get_dividend_total(
+            self.dividend_list, TransactionType.DIVIDEND_IN_LIEU
+        )
+        total_dividend = dividend + dividend_in_lieu
+        withholding_tax = self.get_dividend_total(
+            self.dividend_list, TransactionType.WITHHOLDING
+        )
+        net_income = total_dividend - withholding_tax
+        output_string = (
+            f"Total dividends from {self.tax_year_start} to {self.tax_year_end}:\n"
+        )
+        output_string += (
+            f"Total Dividends: £{total_dividend}\n"
+            f"Foreign withholding tax: £{withholding_tax}\n"
+            f"Net income received: £{net_income}\n\n"
+        )
+        return output_string
+
+    @staticmethod
+    def get_dividend_total(
+        dividend_list: List[Dividend], transaction_type: TransactionType
+    ) -> Decimal:
+        """get the sum of value in Sterling regardless of transaction type"""
+        return sum(
+            [
+                dividend.value.get_value()
+                for dividend in dividend_list
+                if dividend.transaction_type == transaction_type
+            ],
+            Decimal(0),
+        )
+
+    def show_dividend_by_country(self) -> str:
+        """To show dividend summary grouped by the companies' country"""
+        output_string = (
+            f"Dividends by country from {self.tax_year_start} to {self.tax_year_end}:\n"
+        )
+        for country, dividend_list in self.get_dividend_by_country().items():
+            dividend = self.get_dividend_total(dividend_list, TransactionType.DIVIDEND)
+            dividend_in_lieu = self.get_dividend_total(
+                dividend_list, TransactionType.DIVIDEND_IN_LIEU
+            )
+            total_dividend = dividend + dividend_in_lieu
+            withholding_tax = self.get_dividend_total(
+                dividend_list, TransactionType.WITHHOLDING
+            )
+            net_income = total_dividend - withholding_tax
+            output_string += (
+                f"Dividends for {country}:\n"
+                f"Dividends: £{dividend}\n"
+                f"Dividends in lieu: £{dividend_in_lieu}\n"
+                f"Total Dividends: £{total_dividend}\n"
+                f"Foreign withholding tax: £{withholding_tax}\n"
+                f"Net income received: £{net_income}\n\n"
+            )
+        return output_string
---
+++ tests/test_dividend_summary.py
@@ -0,0 +1,126 @@
+""" To test dividend summary """
+import datetime
+from decimal import Decimal
+import unittest
+
+from iso4217 import Currency
+
+from capital_gain.dividend_summary import DividendSummary
+from capital_gain.model import Dividend, Money, TransactionType
+
+
+class TestCalculator(unittest.TestCase):
+    """To test dividend summary"""
+
+    def setUp(self) -> None:
+        """create dividend objects for test"""
+        self.dividends = [
+            Dividend(
+                "ABC",
+                datetime.date(2021, 4, 5),
+                TransactionType.DIVIDEND,
+                Money(Decimal(100), Decimal("0.5"), Currency("USD")),
+                "USA",
+            ),
+            Dividend(
+                "ABC",
+                datetime.date(2021, 4, 6),
+                TransactionType.DIVIDEND,
+                Money(Decimal(200), Decimal("0.5"), Currency("USD")),
+                "USA",
+            ),
+            Dividend(
+                "ABC",
+                datetime.date(2021, 4, 6),
+                TransactionType.WITHHOLDING,
+                Money(Decimal(-20), Decimal("0.5"), Currency("USD")),
+                "USA",
+            ),
+            Dividend(
+                "EFG",
+                datetime.date(2021, 5, 5),
+                TransactionType.DIVIDEND,
+                Money(Decimal(250), Decimal("2"), Currency("CAD")),
+                "CAN",
+            ),
+            Dividend(
+                "EFG",
+                datetime.date(2021, 8, 5),
+                TransactionType.DIVIDEND,
+                Money(Decimal(500), Decimal("2"), Currency("CAD")),
+                "CAN",
+            ),
+            Dividend(
+                "EFG",
+                datetime.date(2021, 8, 5),
+                TransactionType.WITHHOLDING,
+                Money(Decimal(-200), Decimal("2"), Currency("CAD")),
+                "CAN",
+            ),
+            Dividend(
+                "GHI",
+                datetime.date(2021, 4, 20),
+                TransactionType.DIVIDEND_IN_LIEU,
+                Money(Decimal(100), Decimal("0.2"), Currency("HKD")),
+                "HKG",
+            ),
+            Dividend(
+                "GHI",
+                datetime.date(2022, 4, 5),
+                TransactionType.DIVIDEND,
+                Money(Decimal(200), Decimal("0.2"), Currency("HKD")),
+                "HKG",
+            ),
+            Dividend(
+                "GHI",
+                datetime.date(2022, 4, 6),
+                TransactionType.DIVIDEND,
+                Money(Decimal(300), Decimal("0.2"), Currency("HKD")),
+                "HKG",
+            ),
+        ]
+
+    def test_dividend_by_country(self):
+        """To test the text output of dividend summary by country"""
+        data = DividendSummary(
+            self.dividends, datetime.date(2021, 4, 6), datetime.date(2022, 4, 5)
+        )
+        result = data.show_dividend_by_country()
+        expected_output = """Dividends by country from 2021-04-06 to 2022-04-05:
+Dividends for USA:
+Dividends: £100.0
+Dividends in lieu: £0
+Total Dividends: £100.0
+Foreign withholding tax: £-10.0
+Net income received: £110.0
+
+Dividends for CAN:
+Dividends: £1500
+Dividends in lieu: £0
+Total Dividends: £1500
+Foreign withholding tax: £-400
+Net income received: £1900
+
+Dividends for HKG:
+Dividends: £40.0
+Dividends in lieu: £20.0
+Total Dividends: £60.0
+Foreign withholding tax: £0
+Net income received: £60.0
+
+"""
+        self.assertEqual(expected_output, result)
+
+    def test_dividend_sum(self):
+        """To test summing up of dividends in correct period"""
+        data = DividendSummary(
+            self.dividends, datetime.date(2021, 4, 6), datetime.date(2022, 4, 5)
+        )
+        result = data.show_dividend_total()
+        expected_result = """Total dividends from 2021-04-06 to 2022-04-05:
+Total Dividends: £1660.0
+Foreign withholding tax: £-410.0
+Net income received: £2070.0
+
+"""
+        self.assertEqual(expected_result, result)
