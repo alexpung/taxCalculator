efadc7bd00a7bab46a7841c9c36085b3e6997da0
Alex Pun <alexpung@protonmail.com>
2022-05-15 18:19:18 +0100

    Add tax year constant

--- const.py
+++ const.py
@@ -1,2 +1,17 @@
 """constants"""
+import datetime
+
 CONFIG_FILE = "init.toml"
+
+
+def get_tax_year(date: datetime.date) -> int:
+    """return the tax year given a date"""
+    if date.month == 4:
+        if date.day <= 5:
+            return date.year - 1
+        else:
+            return date.year
+    elif date.month < 4:
+        return date.year - 1
+    else:
+        return date.year
