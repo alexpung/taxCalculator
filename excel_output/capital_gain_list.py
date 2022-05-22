c6c8a02b8f1c6399d635925e73a9f055a901f190
Alex Pun <alexpung@protonmail.com>
2022-05-22 11:54:20 +0100

    Implement section 104 and short share excel output Rename write_capital_gain_list to write_capital_gain_excel and refactor code to two functions _write_cgt_per_year_and_summary and _write_trade_by_ticker

--- excel_output/capital_gain_list.py
+++ excel_output/capital_gain_list.py
@@ -11,36 +11,80 @@ from capital_gain.capital_summary import (
     get_number_of_disposal,
     get_total_gain_exclude_loss,
 )
-from capital_gain.model import SellTrade, ShareReorg, Trade, Transaction
+from capital_gain.model import (
+    Section104,
+    Section104Value,
+    SellTrade,
+    ShareReorg,
+    Trade,
+    Transaction,
+)
 from const import get_tax_year
 from excel_output.utility import make_table
 
 
-def write_capital_gain_list(transaction_list: list[Transaction]):
+def write_capital_gain_excels(
+    transaction_list: list[Transaction], section104: Section104
+):
     """Write a list of trades and capital gain summary to a file"""
-    trade_workbook = xlsxwriter.Workbook(
-        "Trades.xlsx", {"default_date_format": "d mmm yyyy"}
-    )
-    cgt_workbook = xlsxwriter.Workbook(
-        "CapitalGainSummary.xlsx", {"default_date_format": "d mmm yyyy"}
-    )
     transaction_list.sort()
+    _write_trade_by_ticker(transaction_list)
+    _write_cgt_per_year_and_summary(transaction_list)
+    _write_section104(section104)
+
+
+def _write_trade_by_ticker(transaction_list: list[Transaction]):
+    trade_workbook = xlsxwriter.Workbook(
+        "TradesByTicker.xlsx", {"default_date_format": "d mmm yyyy"}
+    )
     grouped_list_ticker: defaultdict[str, list[Transaction]] = defaultdict(list)
+    for transaction in transaction_list:
+        grouped_list_ticker[transaction.ticker].append(transaction)
+    for ticker, grouped_list in grouped_list_ticker.items():
+        make_table(trade_workbook, ticker, map(_set_trade_data, grouped_list))
+    trade_workbook.close()
+
+
+def _write_cgt_per_year_and_summary(transaction_list: list[Transaction]):
+    cgt_workbook = xlsxwriter.Workbook(
+        "CgtPerYearAndSummary.xlsx", {"default_date_format": "d mmm yyyy"}
+    )
     grouped_list_year: defaultdict[int, list[Transaction]] = defaultdict(list)
     summary_data = []
     for transaction in transaction_list:
-        grouped_list_ticker[transaction.ticker].append(transaction)
         grouped_list_year[get_tax_year(transaction.transaction_date)].append(
             transaction
         )
-    for ticker, grouped_list in grouped_list_ticker.items():
-        make_table(trade_workbook, ticker, map(_set_trade_data, grouped_list))
     for year, grouped_list in grouped_list_year.items():
         sell_trades = [x for x in grouped_list if isinstance(x, SellTrade)]
         summary_data.append(_set_capital_gain_summary(year, sell_trades))
+        make_table(cgt_workbook, str(year), map(_set_trade_data, grouped_list))
     make_table(cgt_workbook, "Summary", summary_data)
-    trade_workbook.close()
     cgt_workbook.close()
+
+
+def _write_section104(section104: Section104):
+    section104_workbook = xlsxwriter.Workbook(
+        "Section104.xlsx", {"default_date_format": "d mmm yyyy"}
+    )
+    table_list = []
+    for item in section104.section104_list.items():
+        table_list.append(_set_section104(*item))
+    make_table(section104_workbook, "Section104", table_list)
+    make_table(
+        section104_workbook, "Short trade", map(_set_trade_data, section104.short_list)
+    )
+    section104_workbook.close()
+
+
+def _set_section104(
+    section104_key: str, section104_value: Section104Value
+) -> dict[str, Any]:
+    return {
+        "Symbol": section104_key,
+        "Quantity": section104_value.quantity,
+        "Allowable cost": section104_value.cost,
+    }
 
 
 def _set_capital_gain_summary(
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from capital_gain.calculator import CgtCalculator
 from capital_gain.dividend_summary import get_dividend_summary
 from capital_gain.model import BuyTrade, Dividend, Section104, SellTrade, ShareReorg
 import const
-from excel_output.capital_gain_list import write_capital_gain_list
+from excel_output.capital_gain_list import write_capital_gain_excels
 from excel_output.dividend_list import write_dividend_list
 import exception
 from statement_parser.ibkr import (
@@ -32,10 +32,13 @@ class TaxCalculator:
         self.read_section104_from_toml()
         self.load_files(self.select_directory())
         self.calculate()
+
         write_dividend_list(
             self.dividend_list, get_dividend_summary(self.dividend_list)
         )
-        write_capital_gain_list([*self.trades_list, *self.corp_action_list])
+        write_capital_gain_excels(
+            [*self.trades_list, *self.corp_action_list], self.section104
+        )
 
     def read_section104_from_toml(self) -> None:
         """Reading section 104 init data from toml file"""
