917c28d9bde0508a0f01723bb6939f825d08c5b9
Alex Pun <alexpung@protonmail.com>
2022-05-31 14:37:42 +0100

    implement date filter and fx transaction include/exclude option

--- exception.py
+++ exception.py
@@ -1,7 +1,7 @@
 """Error handling code"""
 
 
-def section_104_init_not_found():
+def setting_file_not_found():
     """handling when no init file for section 104"""
     print(
         "Section104 initialization file (init.toml) not found, "
@@ -9,15 +9,16 @@ def section_104_init_not_found():
     )
 
 
-def section104_key_incorrect_or_not_found():
+def section104_incorrect():
     """Handling when section104 key is incorrect or not found"""
     print(
-        "Key for section 104 not found/incorrect, "
+        "Format for section 104 not found/incorrect, "
         "heading should be [['Section104']], "
         "keys are 'symbol', 'quantity', 'value'"
+        "pool will be calculated only from trade"
     )
 
 
-def toml_heading_incorrect():
-    """Handling when toml heading is not correct"""
-    raise ImportError("Heading incorrect: " "heading should be '[[Section104]]'")
+def setting_not_found():
+    """Handling when setting config is incorrect or not found"""
+    print("Configuration not found, will use default setting")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """Main executable file for the project"""
+from datetime import date, datetime
 from decimal import Decimal
 from glob import glob
 from tkinter import Tk, filedialog
+from typing import Optional
 
-from tomlkit import parse
+from tomlkit import TOMLDocument, parse
 from tomlkit.exceptions import NonExistentKey
 from tomlkit.items import AoT
 
@@ -30,12 +32,15 @@ class UKTaxCalculator:
         self.trades_list: list[BuyTrade | SellTrade] = []
         self.corp_action_list: list[ShareReorg] = []
         self.dividend_list: list[Dividend] = []
-        self.read_section104_from_toml()
+        self.include_fx: bool = True
+        self.start_date: Optional[date] = None
+        self.end_date: Optional[date] = None
+        self.read_setting_from_toml()
         self.load_files(self.select_directory())
         # Acquisitions and disposals of GBP is not taxable, so including it is redundant
         self.trades_list = [x for x in self.trades_list if x.ticker != "GBP"]
         self.calculate()
-
+        self.filter_report_by_date()
         write_dividend_list(
             self.dividend_list, get_dividend_summary(self.dividend_list)
         )
@@ -43,25 +48,70 @@ class UKTaxCalculator:
             [*self.trades_list, *self.corp_action_list], self.section104
         )
 
-    def read_section104_from_toml(self) -> None:
-        """Reading section 104 init data from toml file"""
+    def filter_report_by_date(self) -> None:
+        """filter report by start and end date if configured"""
+        if self.start_date:
+            self.trades_list = [
+                x for x in self.trades_list if x.transaction_date >= self.start_date
+            ]
+            self.dividend_list = [
+                x for x in self.dividend_list if x.transaction_date >= self.start_date
+            ]
+            self.corp_action_list = [
+                x
+                for x in self.corp_action_list
+                if x.transaction_date >= self.start_date
+            ]
+        if self.end_date:
+            self.trades_list = [
+                x for x in self.trades_list if x.transaction_date <= self.end_date
+            ]
+            self.dividend_list = [
+                x for x in self.dividend_list if x.transaction_date <= self.end_date
+            ]
+            self.corp_action_list = [
+                x for x in self.corp_action_list if x.transaction_date <= self.end_date
+            ]
+
+    def read_setting_from_toml(self) -> None:
+        """Reading toml file"""
         try:
             with open(const.CONFIG_FILE, encoding="utf-8") as config_file:
                 parsed_content = parse(config_file.read())
-                section104 = parsed_content["Section104"]
-                if isinstance(section104, AoT):
-                    for entry in section104.value:
-                        self.section104.add_to_section104(
-                            entry["symbol"],
-                            Decimal(entry["quantity"]),
-                            Decimal(entry["value"]),
-                        )
-                else:
-                    exception.toml_heading_incorrect()
+                self._read_section104_from_toml(parsed_content)
+                self._read_config_from_toml(parsed_content)
         except FileNotFoundError:
-            exception.section_104_init_not_found()
+            exception.setting_file_not_found()
+
+    def _read_config_from_toml(self, parsed_content: TOMLDocument):
+        """Reading configuration setting from toml file"""
+        settings = parsed_content.get("Settings")
+        if not settings:
+            exception.setting_not_found()
+            return
+        start_date = settings.get("reporting_period_start")
+        end_date = settings.get("reporting_period_end")
+        self.include_fx = settings.get("include_fx")
+        if start_date:
+            self.start_date = datetime.strptime(start_date, "%d-%b-%Y").date()
+        if end_date:
+            self.end_date = datetime.strptime(end_date, "%d-%b-%Y").date()
+
+    def _read_section104_from_toml(self, parsed_content: TOMLDocument):
+        """reading section104 initial state from toml file"""
+        try:
+            section104 = parsed_content["Section104"]
+            if isinstance(section104, AoT):
+                for entry in section104.value:
+                    self.section104.add_to_section104(
+                        entry["symbol"],
+                        Decimal(entry["quantity"]),
+                        Decimal(entry["value"]),
+                    )
+            else:
+                exception.section104_incorrect()
         except NonExistentKey:
-            exception.section104_key_incorrect_or_not_found()
+            exception.section104_incorrect()
 
     @staticmethod
     def select_directory() -> list[str]:
@@ -79,7 +129,8 @@ class UKTaxCalculator:
         for file in file_list:
             self.trades_list.extend(parse_trade(file))
             self.corp_action_list.extend(parse_corp_action(file))
-            self.trades_list.extend(parse_fx_acquisition_and_disposal(file))
+            if self.include_fx:
+                self.trades_list.extend(parse_fx_acquisition_and_disposal(file))
             self.dividend_list.extend(parse_dividend(file))
 
     def calculate(self) -> None:
